f9033d87958c513cd75f340eb9927c609411128d
cklynch2 <cklynch2@gmail.com>
2016-06-15 16:52:10 -0400

    Poodle, Lassie and Beethoven are all dogs even if they started out as generic pets

--- objc-PetCast/FISAppDelegate.m
+++ objc-PetCast/FISAppDelegate.m
@@ -1,10 +1,33 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISPet.h"
+#import "FISDog.h"
 
 @implementation FISAppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    FISDog *poodle = [[FISDog alloc] init];
+    NSLog(@"Can the poodle bark? %@", [poodle makeASound]);
+    NSLog(@"Can the poodle assault the mailman? %@", [poodle assaultTheMailman]);
+    FISPet *poodlePet = (FISPet *)poodle;
+    NSLog(@"Can the Pet poodle still bark? %@", [poodlePet makeASound]);
+    
+    
+    // Calling methods that are unique to the FISDog subclass does NOT work on objects of the FISPet superclass, even if you cast them to FISDog.
+    FISPet *lassie = [[FISPet alloc] init];
+    FISDog *lassieDog = (FISDog *)lassie;
+    NSLog(@"What sound does the Lassie pseudo-dog Pet make? %@", [lassieDog makeASound]);
+//    NSLog(@"What happens if Lassie tries to assault the mailman? %@", [lassieDog assaultTheMailman]);
+    
+    
+    // Instead, you can initialize an FISDog object and assign it to an FISPet variable. This means that we can access the properties and methods that are unique to the FISDog class.
+    FISPet *beethoven = [[FISDog alloc] init];
+    FISDog *beethovenDog = (FISDog *)beethoven;
+    NSLog(@"Beethoven is a FISDog inside a FISPet variable! Can he assault the mailman? %@", [beethovenDog
+                                                                                              assaultTheMailman]);
+    
     return YES;
 }
 
---
+++ objc-PetCast/FISDog.m
@@ -0,0 +1,23 @@
+//
+//  FISDog.m
+//  objc-PetCast
+//
+//  Created by Flatiron School on 6/15/16.
+//  Copyright © 2016 The Flatiron School. All rights reserved.
+//
+
+#import "FISDog.h"
+
+@implementation FISDog
+
+-(NSString *)makeASound {
+    NSString *bark = @"Woof!";
+    return bark;
+}
+
+-(NSString *)assaultTheMailman {
+    NSString *mail = @"I got the mail! I got the mail!";
+    return mail;
+}
+
+@end
